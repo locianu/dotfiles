aa5b6743745442813e5e66df4b359865f5a6f7b5
an0luz_doma <lucianojuniordoliver16@gmail.com>
2025-09-03 23:08:27 -0300

    add new configs + lsp working

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ vim.o.swapfile = true
 vim.o.expandtab = true
 vim.o.winborder = "rounded"
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+    vim.o.clipboard = 'unnamedplus'
 end) -- sync nvim and OS clipboard
 
 vim.o.breakindent = true
@@ -38,27 +38,13 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
 })
-vim.spell.set = {'en_us', 'pt_br'}
-
--- keymapping
-vim.keymap.set('n', '<leader>w', ':write<CR>') -- save
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>') -- save + shoutout file
-vim.keymap.set('n', '<leader>q', ':quit<CR>') -- quit
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format) --select language format
-vim.keymap.set('n', '<leader>e', ':Lexplore<CR>') -- file explorer
-vim.keymap.set('n', '<leader>nt', ':tabNext<CR>') -- next tab
-vim.keymap.set('n', '<leader>t', ':tabnew<CR>') -- create new tab
-vim.keymap.set('n', '<leader>bt', ':tabprevious<CR>') -- create new tab
-vim.keymap.set('n', '<leader>b', ':ls<CR>') -- show buffers
-vim.keymap.set('n', '<leader>ba', ':ls a<CR>') -- show active buffers
-vim.keymap.set('n', '<leader>m', ':map<CR>') -- show list of mappings
-vim.keymap.set('i', '<C-c>', '<C-x><C-o>') -- omnicompletion (native)
+vim.spell.set = { 'en_us', 'pt_br' }
 
 -- netrw explorer settings
 vim.g.netrw_bufsettings = 'noma nomod nu nowrap ro nobl'
@@ -72,141 +58,92 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
--- plugins
-vim.pack.add({
-    {src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main'},
-    {src = 'https://github.com/neovim/nvim-lspconfig'},
-    {src = 'https://github.com/mason-org/mason.nvim'},
-    {src = 'https://github.com/lervag/vimtex'},
-    {src = 'https://github.com/nvim-mini/mini.diff'},
-    {src = 'https://github.com/nvim-mini/mini.notify'},
-    {src = 'https://github.com/nvim-mini/mini.deps'},
-    {src = 'https://github.com/nvim-mini/mini.pick'},
---    {src = 'https://github.com/nvim-mini/mini.git'},
-    {src = 'https://github.com/nvim-mini/mini.animate'},
-    {src = 'https://github.com/nvim-mini/mini.icons'},
-    {src = 'https://github.com/nvim-mini/mini.starter'},
-    {src = 'https://github.com/nvim-mini/mini.statusline'},
-    {src = 'https://github.com/nvim-mini/mini.pairs'},
-    {src = 'https://github.com/nvim-mini/mini.surround'},
---    {src = 'https://github.com/nvim-mini/mini.snippets'}
---    {src = ''},
---    {src = ''},
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+vim.lsp.enable({
+    "lua_ls",
+    "basedpyright",
+    "r",
+    "clangd",
+    "css-lsp",
+    "glow",
+    "harper-ls",
+    "html-lsp",
+    "julia-lsp",
+    "ltex-ls-plus",
+    "lua-language-server",
 })
 
-require 'mason'.setup()
---
---vim.lsp.config('*',{
---    root_markers = {'.git'},
---})
---
---vim.lsp.config('lua_ls', {
---    cmd = {'lua-language-server'},
---    filetypes = {'lua'},
---    root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml"},
---      on_init = function(client)
---        if client.workspace_folders then
---          local path = client.workspace_folders[1].name
---          if
---            path ~= vim.fn.stdpath('config')
---            and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
---          then
---            return
---          end
---        end
---        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
---          runtime = {
---            -- Tell the language server which version of Lua you're using (most
---            -- likely LuaJIT in the case of Neovim)
---            version = 'LuaJIT',
---            -- Tell the language server how to find Lua modules same way as Neovim
---            -- (see `:h lua-module-load`)
---            path = {
---              'lua/?.lua',
---              'lua/?/init.lua',
---            },
---          },
---          -- Make the server aware of Neovim runtime files
---          workspace = {
---            checkThirdParty = false,
---            library = {
---              vim.env.VIMRUNTIME
---              -- Depending on the usage, you might want to add additional paths
---              -- here.
---              -- '${3rd}/luv/library'
---              -- '${3rd}/busted/library'
---            }
---            -- Or pull in all of 'runtimepath'.
---            -- NOTE: this is a lot slower and will cause issues when working on
---            -- your own configuration.
---            -- See https://github.com/neovim/nvim-lspconfig/issues/3189
---            -- library = {
---            --   vim.api.nvim_get_runtime_file('', true),
---            -- }
---          }
---        })
---      end,
---      settings = {
---        Lua = {}
---      }
---})
---
---vim.lsp.config('texlab',{
---    cmd = {'texlab'},
---    filetypes = {'tex', 'plaintex', 'bib'},
---    root_markers = {'.latexmkrc', '.texlabroot', 'Tectonic.toml'},
---    settings = {
---      texlab = {
---        bibtexFormatter = "texlab",
---        build = {
---          args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
---          executable = "latexmk",
---          forwardSearchAfter = false,
---          onSave = false
---        },
---        chktex = {
---          onEdit = false,
---          onOpenAndSave = false
---        },
---        diagnosticsDelay = 300,
---        formatterLineLength = 80,
---        forwardSearch = {
---          args = {}
---        },
---        latexFormatter = "latexindent",
---        latexindent = {
---          modifyLineBreaks = false
---        }
---      }
---    },
---})
---
---vim.lsp.config('basedpyright',{
---    cmd = { "basedpyright-langserver", "--stdio" },
---    filetypes = {'python'},
---    root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json"},
---    settings = {
---      basedpyright = {
---        analysis = {
---          autoSearchPaths = true,
---          diagnosticMode = "openFilesOnly",
---          useLibraryCodeForTypes = true
---        }
---      }
---    }
---})
---
---vim.lsp.config('julials',{
---    cmd = { "julia", "--startup-file=no", "--history-file=no", "-e", '    # Load LanguageServer.jl: attempt to load from ~/.julia/environments/nvim-lspconfig\n    # with the regular load path as a fallback\n    ls_install_path = joinpath(\n        get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")),\n        "environments", "nvim-lspconfig"\n    )\n    pushfirst!(LOAD_PATH, ls_install_path)\n    using LanguageServer\n    popfirst!(LOAD_PATH)\n    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")\n    project_path = let\n        dirname(something(\n            ## 1. Finds an explicitly set project (JULIA_PROJECT)\n            Base.load_path_expand((\n                p = get(ENV, "JULIA_PROJECT", nothing);\n                p === nothing ? nothing : isempty(p) ? nothing : p\n            )),\n            ## 2. Look for a Project.toml file in the current working directory,\n            ##    or parent directories, with $HOME as an upper boundary\n            Base.current_project(),\n            ## 3. First entry in the load path\n            get(Base.load_path(), 1, nothing),\n            ## 4. Fallback to default global environment,\n            ##    this is more or less unreachable\n            Base.load_path_expand("@v#.#"),\n        ))\n    end\n    @info "Running language server" VERSION pwd() project_path depot_path\n    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)\n    server.runlinter = true\n    run(server)\n  ' },
---    filetypes = {'julia'},
---    root_markers = { "Project.toml", "JuliaProject.toml" },
---})
---
-----vim.lsp.config('',{})
-----
-----vim.lsp.config('',{
-----})
---
---vim.lsp.enable('lua_ls', 'basedpyright', 'texlab', 'julials')
---
---
+-- plugins
+vim.pack.add({
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = 'https://github.com/lervag/vimtex' },
+    { src = 'https://github.com/nvim-mini/mini.diff' },
+    { src = 'https://github.com/nvim-mini/mini.notify' },
+    { src = 'https://github.com/nvim-mini/mini.pick' },
+    { src = 'https://github.com/nvim-mini/mini.icons' },
+    { src = 'https://github.com/nvim-mini/mini.starter' },
+    { src = 'https://github.com/nvim-mini/mini.statusline' },
+    { src = 'https://github.com/nvim-mini/mini.pairs' },
+    { src = 'https://github.com/nvim-mini/mini.surround' },
+    { src = 'https://github.com/nvim-mini/mini.snippets' }
+    --    {src = ''},
+    --    {src = ''},
+})
+
+require 'mason'.setup {}
+require 'nvim-treesitter.config'.setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "julia", "latex", "css", "r", "ruby", "html" },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+require 'mini.diff'.setup{}
+require 'mini.notify'.setup{}
+require 'mini.pick'.setup{}
+require 'mini.icons'.setup{}
+require 'mini.starter'.setup{}
+require 'mini.statusline'.setup{}
+require 'mini.pairs'.setup{
+    mappings = {
+        ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+        ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+    },
+}
+require 'mini.surround'.setup{}
+require 'mini.snippets'.setup{}
+
+
+-- keymapping
+vim.keymap.set('n', '<leader>w', ':write<CR>')              -- save
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>') -- save + shoutout file
+vim.keymap.set('n', '<leader>q', ':quit<CR>')               -- quit
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)       --select language format
+vim.keymap.set('n', '<leader>e', ':Lexplore<CR>')           -- file explorer
+vim.keymap.set('n', '<leader>nt', ':tabNext<CR>')           -- next tab
+vim.keymap.set('n', '<leader>t', ':tabnew<CR>')             -- create new tab
+vim.keymap.set('n', '<leader>bt', ':tabprevious<CR>')       -- create new tab
+vim.keymap.set('n', '<leader>b', ':ls<CR>')                 -- show buffers
+vim.keymap.set('n', '<leader>ba', ':ls a<CR>')              -- show active buffers
+vim.keymap.set('n', '<leader>m', ':map<CR>')                -- show list of mappings
+vim.keymap.set('i', '<C-c>', '<C-x><C-o>')                  -- omnicompletion (native)
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
